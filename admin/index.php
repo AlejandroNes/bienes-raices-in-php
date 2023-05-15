8c5b57ea64e0348b3c71970938057506bf23e852
AlejandroNes <alejandro315595@mail.com>
2023-05-15 02:31:08 -0400

    version 1 bienes raices

---
+++ admin/index.php
@@ -0,0 +1,18 @@
+<?php
+require '../includes/funciones.php';
+
+incluirTemplate('header');
+
+?>
+
+<main class="contenedor seccion">
+    <h1>Administrador de Bienes Raices </h1>
+
+    <a href="/bienesraices/admin/propiedades/crear.php" class="boton boton-verde">Crear</a>
+</main>
+
+
+<br>
+<?php
+incluirTemplate('footer');
+?>
---
+++ admin/propiedades/crear.php
@@ -0,0 +1,18 @@
+<?php
+require '../../includes/funciones.php';
+
+incluirTemplate('header');
+
+?>
+
+<main class="contenedor seccion">
+    <h1>Crear Propiedades </h1>
+
+    <a href="/bienesraices/admin" class="boton boton-verde">Volver</a>
+</main>
+
+<br>
+
+<?php
+incluirTemplate('footer');
+?>
